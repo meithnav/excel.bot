ed02b20edca00688996f099998ec29c9c9a3b07e
meithnav <meithnavlakha@gmail.com>
2024-03-31 01:57:27 -0700

    updated prompt

--- LLM/chatgpt.py
+++ LLM/chatgpt.py
@@ -17,15 +17,28 @@ class ChatGPT:
         self.openai_client = None
         self.openai_client = OpenAI(api_key=self.OPENAI_API_KEY)
 
-    def llmResponse(self, user_prompt, query=0,  delim=";$;%"):
+    def llmResponse(self, res_dict, query=0,  delim=";$;%"):
+
+        # prompt = f"""Consider you are a Google Sheets expert with 5 years of experience. You know all the formulas and commands for the Google Sheets. You would be given a prompt  and your role is to return the formula for the prompt. If there are more than 1 formula then return as nested arrays: 
+
+        #             Prompt:{res_dict['user_prompt']}"""
+
+        ### updated prompt
+        prompt = f"""Consider you are a Google Sheets expert with 5 years of experience. You know all the formulas and commands for the Google Sheets. You would be given a prompt  and your role is to return the excel formula for the prompt. Donot try to complicate the formula. Simplicity is bliss. Always recheck the formula with a few sample examples if it's working or not. Apply COT. If there are more than 1 formula then add `$;%` as delimiter:
+
+                    Meaning of the KEYWORDS:
+                    FROM_SHEET: From where we are copying the data. Please make sure to include it in the formula where ever data is being read. If there is space in the given FROM_SHEET name then make sure to include a quotes in the formula otherwise it may throw an error.
+                    TO_SHEET : Where we will store the data
+                    OUTPUT_LOCS : The cell in the TO_SHEET where we will paste the formula.
+
+                    Meta-data: {res_dict}
+                    Prompt:{res_dict['user_prompt']}"""
+
         if os.getenv("IS_DEMO"):
             # print("\nDEMO CHATGPT RES: ", prompts[query]['chat_gpt'])
             return prompts[query]['chat_gpt']
         else:
             ## ADD PIPELINE TO LLM##
-            prompt = f"""Consider you are a Google Sheets expert with 5 years of experience. You know all the formulas and commands for the Google Sheets. You would be given a prompt  and your role is to return the formula for the prompt. If there are more than 1 formula then return as nested arrays: 
-
-                    Prompt:{user_prompt}"""
 
             if self.openai_client:
                 response = self.openai_client.chat.completions.create(
